425d7a628490ff83dcd4e59a5eddc8914bdcf1e8
Dev_Lap04 <dev_lap04@Dev-Lap04s-MacBook-Pro.local>
2021-12-23 17:20:26 +0200

    fix: packages design on mobile

--- src/styled/packages/packages.component.tsx
+++ src/styled/packages/packages.component.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { useTranslation } from "react-i18next";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 import styles from "./packages.module.css";
 
@@ -14,123 +16,262 @@ const Packages = (props: Props) => {
     <div className={styles.container}>
       <p className={styles.title}>{t("OUR_PACKAGES").toUpperCase()}</p>
       <p className={styles.subtitle}>{t("CHOOSE_THE_FEATURES")}</p>
-      <div className={styles.row}>
-        <div className={styles.packageContainer}>
-          <p className={styles.packageTitle}>{t("ESSENTIAL")}</p>
-          <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
-          <div className={styles.packageRow}>
-            <p className={styles.packagePriceCurrency}>{t("AED")}</p>
-            <p className={styles.packagePrice}>3.00</p>
-          </div>
-          <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("DIGITAL_ONBOARDING")}</p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("DIGITAL_CARD")}</p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("CARD_CONTROLS")}</p>
-          </div>
-          <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
-          <p className={styles.packageStolenCardCurrency}>{t("AED")} 5</p>
-          <div
-            className={styles.packageButton}
-            style={{ backgroundColor: "#306bdd" }}
-          >
-            <p className={styles.contactUs}>{t("CONTACT_US")}</p>
-          </div>
-          <p className={styles.packageBenefitDescription}>
-            {t("BENEFIT_MESSAGE_ONE")}
-            <p className={styles.packageBenefitUsers}>
-              {t("UP_TO_FIVE_USERS")}
+      {window.innerWidth > 768 ? (
+        <div className={styles.row}>
+          <div className={styles.packageContainer}>
+            <p className={styles.packageTitle}>{t("ESSENTIAL")}</p>
+            <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+            <div className={styles.packageRow}>
+              <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+              <p className={styles.packagePrice}>3.00</p>
+            </div>
+            <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("DIGITAL_ONBOARDING")}
+              </p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>{t("DIGITAL_CARD")}</p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>{t("CARD_CONTROLS")}</p>
+            </div>
+            <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+            <p className={styles.packageStolenCardCurrency}>{t("AED")} 5</p>
+            <div
+              className={styles.packageButton}
+              style={{ backgroundColor: "#306bdd" }}
+            >
+              <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+            </div>
+            <p className={styles.packageBenefitDescription}>
+              {t("BENEFIT_MESSAGE_ONE")}
+              <p className={styles.packageBenefitUsers}>
+                {t("UP_TO_FIVE_USERS")}
+              </p>
             </p>
-          </p>
+          </div>
+          <div className={styles.middlePackageContainer}>
+            <p className={styles.packageTitle} style={{ marginTop: 30 }}>
+              {t("PRO")}
+            </p>
+            <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+            <div className={styles.packageRow}>
+              <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+              <p className={styles.packagePrice}>5.00</p>
+            </div>
+            <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("PHYSICAL_CARD_REQUEST")}
+              </p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("PRIORITY_IN_CALL_CENTER")}
+              </p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("AUTOMATED_FUND_RELOAD")}
+              </p>
+            </div>
+            <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+            <p className={styles.packageStolenCardCurrency}>
+              {t("FREE_UP_TO_THREE")}
+            </p>
+            <div
+              className={styles.packageButton}
+              style={{ backgroundColor: "#30c675" }}
+            >
+              <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+            </div>
+            <p className={styles.packageBenefitDescription}>
+              {t("BENEFIT_MESSAGE_TWO")}
+              <p className={styles.packageBenefitUsers}>
+                {t("UP_TO_TEN_USERS")}
+              </p>
+            </p>
+          </div>
+          <div className={styles.packageContainer}>
+            <p className={styles.packageTitle}>{t("ENTERPRISE")}</p>
+            <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+            <div className={styles.packageRow}>
+              <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+              <p className={styles.packagePrice}>10.00</p>
+            </div>
+            <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("DEDICATED_SUPPORT_AGENT")}
+              </p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("MULTIPLE_ADMIN_ACCESS")}
+              </p>
+            </div>
+            <div className={styles.packageBeneftisRow}>
+              <img src={SignTwoLogo} className={styles.imageContainer} />
+              <p className={styles.packageBenefits}>
+                {t("MULTIPLE_ENTITY_CONSOLIDATION")}
+              </p>
+            </div>
+            <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+            <p className={styles.packageStolenCardCurrency}>
+              {t("FREE_UP_TO_FIVE")}
+            </p>
+            <div
+              className={styles.packageButton}
+              style={{ backgroundColor: "#00194a" }}
+            >
+              <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+            </div>
+            <p className={styles.packageBenefitDescription}>
+              <p className={styles.packageBenefitUsers}>{t("UNLIMITED")}</p>
+            </p>
+          </div>
         </div>
-        <div
-          className={styles.packageContainer}
-          style={{ height: 509, marginTop: 0 }}
-        >
-          <p className={styles.packageTitle} style={{ marginTop: 30 }}>
-            {t("PRO")}
-          </p>
-          <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
-          <div className={styles.packageRow}>
-            <p className={styles.packagePriceCurrency}>{t("AED")}</p>
-            <p className={styles.packagePrice}>5.00</p>
-          </div>
-          <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>
-              {t("PHYSICAL_CARD_REQUEST")}
-            </p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>
-              {t("PRIORITY_IN_CALL_CENTER")}
-            </p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>
-              {t("AUTOMATED_FUND_RELOAD")}
-            </p>
-          </div>
-          <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
-          <p className={styles.packageStolenCardCurrency}>
-            {t("FREE_UP_TO_THREE")}
-          </p>
-          <div
-            className={styles.packageButton}
-            style={{ backgroundColor: "#30c675" }}
-          >
-            <p className={styles.contactUs}>{t("CONTACT_US")}</p>
-          </div>
-          <p className={styles.packageBenefitDescription}>
-            {t("BENEFIT_MESSAGE_TWO")}
-            <p className={styles.packageBenefitUsers}>{t("UP_TO_TEN_USERS")}</p>
-          </p>
+      ) : (
+        <div style={{marginLeft: "7.5%"}}>
+          <Carousel showThumbs={false} showStatus={false}>
+            <div className={styles.packageContainer}>
+              <p className={styles.packageTitle}>{t("ESSENTIAL")}</p>
+              <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+              <div className={styles.packageRow}>
+                <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+                <p className={styles.packagePrice}>3.00</p>
+              </div>
+              <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+        
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+           
+                <p className={styles.packageBenefits}>
+                  {t("DIGITAL_ONBOARDING")}
+                </p>
+         
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>{t("DIGITAL_CARD")}</p>
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>{t("CARD_CONTROLS")}</p>
+              </div>
+              <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+              <p className={styles.packageStolenCardCurrency}>{t("AED")} 5</p>
+              <div
+                className={styles.packageButton}
+                style={{ backgroundColor: "#306bdd" }}
+              >
+                <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+              </div>
+              <p className={styles.packageBenefitDescription}>
+                {t("BENEFIT_MESSAGE_ONE")}
+                <p className={styles.packageBenefitUsers}>
+                  {t("UP_TO_FIVE_USERS")}
+                </p>
+              </p>
+            </div>
+            <div className={styles.middlePackageContainer}>
+              <p className={styles.packageTitle} style={{ marginTop: 30 }}>
+                {t("PRO")}
+              </p>
+              <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+              <div className={styles.packageRow}>
+                <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+                <p className={styles.packagePrice}>5.00</p>
+              </div>
+              <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("PHYSICAL_CARD_REQUEST")}
+                </p>
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("PRIORITY_IN_CALL_CENTER")}
+                </p>
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("AUTOMATED_FUND_RELOAD")}
+                </p>
+              </div>
+              <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+              <p className={styles.packageStolenCardCurrency}>
+                {t("FREE_UP_TO_THREE")}
+              </p>
+              <div
+                className={styles.packageButton}
+                style={{ backgroundColor: "#30c675" }}
+              >
+                <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+              </div>
+              <p className={styles.packageBenefitDescription}>
+                {t("BENEFIT_MESSAGE_TWO")}
+                <p className={styles.packageBenefitUsers}>
+                  {t("UP_TO_TEN_USERS")}
+                </p>
+              </p>
+            </div>
+            <div className={styles.packageContainer}>
+              <p className={styles.packageTitle}>{t("ENTERPRISE")}</p>
+              <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
+              <div className={styles.packageRow}>
+                <p className={styles.packagePriceCurrency}>{t("AED")}</p>
+                <p className={styles.packagePrice}>10.00</p>
+              </div>
+              <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("DEDICATED_SUPPORT_AGENT")}
+                </p>
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("MULTIPLE_ADMIN_ACCESS")}
+                </p>
+              </div>
+              <div className={styles.packageBeneftisRow}>
+                <img src={SignTwoLogo} className={styles.imageContainer} />
+                <p className={styles.packageBenefits}>
+                  {t("MULTIPLE_ENTITY_CONSOLIDATION")}
+                </p>
+              </div>
+              <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
+              <p className={styles.packageStolenCardCurrency}>
+                {t("FREE_UP_TO_FIVE")}
+              </p>
+              <div
+                className={styles.packageButton}
+                style={{ backgroundColor: "#00194a" }}
+              >
+                <p className={styles.contactUs}>{t("CONTACT_US")}</p>
+              </div>
+              <p className={styles.packageBenefitDescription}>
+                <p className={styles.packageBenefitUsers}>{t("UNLIMITED")}</p>
+              </p>
+            </div>
+          </Carousel>
         </div>
-        <div className={styles.packageContainer}>
-          <p className={styles.packageTitle}>{t("ENTERPRISE")}</p>
-          <p className={styles.packageStartingAt}>{t("STARTING_AT")}</p>
-          <div className={styles.packageRow}>
-            <p className={styles.packagePriceCurrency}>{t("AED")}</p>
-            <p className={styles.packagePrice}>10.00</p>
-          </div>
-          <p className={styles.packageUserMonth}>{t("USER_MONTH")}</p>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("DEDICATED_SUPPORT_AGENT")}</p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("MULTIPLE_ADMIN_ACCESS")}</p>
-          </div>
-          <div className={styles.packageBeneftisRow}>
-            <img src={SignTwoLogo} style={{ objectFit: "contain" }} />
-            <p className={styles.packageBenefits}>{t("MULTIPLE_ENTITY_CONSOLIDATION")}</p>
-          </div>
-          <p className={styles.packageStolenCard}>{t("STOLEN_CARD_FEE")}</p>
-          <p className={styles.packageStolenCardCurrency}>{t("FREE_UP_TO_FIVE")}</p>
-          <div
-            className={styles.packageButton}
-            style={{ backgroundColor: "#00194a" }}
-          >
-            <p className={styles.contactUs}>{t("CONTACT_US")}</p>
-          </div>
-          <p className={styles.packageBenefitDescription}>
-            <p className={styles.packageBenefitUsers}>
-              {t("UNLIMITED")}
-            </p>
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
